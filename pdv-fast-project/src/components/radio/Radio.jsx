24a574b59900eb0fd6243bdafa412886596fde3c
anaporpino <anatc30@gmail.com>
2025-12-04 20:24:09 -0300

    Adicionando arquivos

--- pdv-fast-project/src/components/radio/Radio.jsx
+++ pdv-fast-project/src/components/radio/Radio.jsx
@@ -1,1 +1,18 @@
 //Componente de radio button.
+const Radio = ({ label, name, value, checked, onChange }) => {
+  return (
+    <label className="flex items-center space-x-2">
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        className="form-radio h-4 w-4 accent-orange-500"
+      />
+      <span>{label}</span>
+    </label>
+  );
+};
+
+export default Radio;
